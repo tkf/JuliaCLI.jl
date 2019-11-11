469117761ac596d1350035134196ddf6db17e0f4
Takafumi Arakaki <aka.tkf@gmail.com>
2019-11-10 20:12:49 -0800

    Run tests in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
-using JuliaCLI
-using Test
+import JuliaCLI
 
-@testset "JuliaCLI.jl" begin
-    # Write your own tests here.
+@async JuliaCLI.serve()  # TODO: don't
+let path = joinpath(DEPOT_PATH[1], "jlcli", "socket")
+    for i in 1:100
+        isfile(path) && break
+        sleep(0.1)
+    end
 end
+
+include("cli_test.jl")
